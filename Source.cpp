cb902e38f099b066aace00aa37afcc45e6690d0a
0raqu666 <49256096+0raqu666@users.noreply.github.com>
2019-04-05 14:48:56 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -5,13 +5,9 @@ using namespace std;
 
 class Korobki
 {
-private:
-		int weidht, leinght, height;
 public:
-	/*Korobki(int cm_weidht, int cm_leinght, int cm_height)
-	{
-		setVvod(cm_weidht, cm_leinght, cm_height);
-	}*/
+	
+	int weidht, leinght, height;
 
 	void Vvod()
 	{
@@ -22,52 +18,20 @@ public:
 		cout << "Visota=";
 		cin >> height;
 	}
-	void setVvod(int cm_weidht, int cm_leinght, int cm_height)
-	{
-		weidht = cm_weidht;
-		leinght = cm_leinght;
-		height = cm_height;
-	}
 };
 
 
 int main()
 {
-	int weidht, leinght, height;
-	int x, y, z,x1,y1,z1;
 	Korobki korobka1;
 	Korobki	korobka2;
-
-	cout << "Shirina=";
-	cin >> weidht;
-	cout << "Dlina=";
-	cin >> leinght;
-	cout << "Visota=";
-	cin >> height;
-
-	korobka1.setVvod(weidht, leinght, height);
-
-	x = weidht, y = leinght, z = height;
-
+	korobka1.Vvod();
 	cout << "Korobka 2\n";
-
-	cout << "Shirina=";
-	cin >> weidht;
-	cout << "Dlina=";
-	cin >> leinght;
-	cout << "Visota=";
-	cin >> height;
-
-	korobka2.setVvod(weidht, leinght, height);
-
-	x1 = weidht, y1 = leinght, z1 = height;
-
-	if (x > x1&&y > y1&&z > z1)
+	korobka2.Vvod();
+	if (korobka1.weidht>korobka2.weidht&&korobka1.height > korobka2.height&&korobka1.leinght>korobka2.leinght)
 		cout << "Vhodit";
 	else
 		cout << "Nevhodit";
-
-
 	_getch();
 	return 0;
 }
